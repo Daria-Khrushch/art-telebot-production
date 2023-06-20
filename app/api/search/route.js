c45b9293e9b085105135e9bbf299dac85d1e8d59
Daria Khrushch <73955564+Daria-Khrushch@users.noreply.github.com>
2023-06-20 10:21:17 +0100

    add geo and type filter

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -13,51 +13,199 @@ export const GET = async (req, res) => {
     // console.log(query);
     // console.log(geo)
     // console.log(type)
-    if (title && !theme && !language) {
+    if (title && !theme && !language && !geo && !type) {
       const channels = await prisma.channel.findMany({
-        where: {name: title || ""},
+        where: { name: title || "" },
       });
       return new Response(JSON.stringify(channels), { status: 200 });
-    } else if (theme && !title && !language) {
+    } else if (theme && !title && !language && !geo && !type) {
       const channels = await prisma.channel.findMany({
-        where: {theme: theme || ""},
+        where: { theme: theme || "" },
       });
       return new Response(JSON.stringify(channels), { status: 200 });
-    } else if (language && !theme && !title) {
+    } else if (language && !theme && !title && !geo && !type) {
       const channels = await prisma.channel.findMany({
-        where: { language: language || ""},
+        where: { language: language || "" },
       });
       return new Response(JSON.stringify(channels), { status: 200 });
-    } else if (language && theme && !title) {
+    } else if (language && theme && !title && !geo && !type) {
       const channels = await prisma.channel.findMany({
         where: {
-              theme: theme || "",
-              language: language || ""
+          theme: theme || "",
+          language: language || "",
         },
       });
       return new Response(JSON.stringify(channels), { status: 200 });
-    } else if (title && theme && !language) {
+    } else if (title && theme && !language && !geo && !type) {
       const channels = await prisma.channel.findMany({
         where: {
-              name: title || "",
-              theme: theme || ""
+          name: title || "",
+          theme: theme || "",
         },
       });
       return new Response(JSON.stringify(channels), { status: 200 });
-    } else if (language && title && !theme) {
+    } else if (language && title && !theme && !geo && !type) {
       const channels = await prisma.channel.findMany({
         where: {
-              name: title || "",
-              language: language || ""
+          name: title || "",
+          language: language || "",
         },
       });
       return new Response(JSON.stringify(channels), { status: 200 });
-    } else if (language && title && theme) {
+    } else if (language && title && theme && !geo && !type) {
       const channels = await prisma.channel.findMany({
         where: {
           name: title || "",
           theme: theme || "",
           language: language || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (geo && !title && !theme && !language && !type) {
+      const channels = await prisma.channel.findMany({
+        where: { geolocation: geo || "" },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (type && !title && !theme && !language && !geo) {
+      const channels = await prisma.channel.findMany({
+        where: { type: type || "" },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (geo && type && !title && !theme && !language) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          geolocation: geo || "",
+          type: type || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (title && geo && !theme && !language && !type) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          name: title || "",
+          geolocation: geo || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (title && type && !theme && !language && !geo) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          name: title || "",
+          type: type || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (theme && geo && !title && !language && !type) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          theme: theme || "",
+          geolocation: geo || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (theme && type && !title && !language && !geo) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          theme: theme || "",
+          type: type || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (language && geo && !title && !theme && !type) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          language: language || "",
+          geolocation: geo || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (language && type && !title && !theme && !geo) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          language: language || "",
+          type: type || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (geo && type && !title && !theme && !language) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          geolocation: geo || "",
+          type: type || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (title && theme && geo && !language && !type) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          name: title || "",
+          theme: theme || "",
+          geolocation: geo || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (title && theme && type && !language && !geo) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          name: title || "",
+          theme: theme || "",
+          type: type || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (title && language && geo && !theme && !type) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          name: title || "",
+          language: language || "",
+          geolocation: geo || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (title && language && type && !theme && !geo) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          name: title || "",
+          language: language || "",
+          type: type || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (theme && language && geo && !title && !type) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          theme: theme || "",
+          language: language || "",
+          geolocation: geo || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (theme && language && type && !title && !geo) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          theme: theme || "",
+          language: language || "",
+          type: type || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (language && title && theme && geo && !type) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          name: title || "",
+          theme: theme || "",
+          language: language || "",
+          geolocation: geo || "",
+        },
+      });
+      return new Response(JSON.stringify(channels), { status: 200 });
+    } else if (language && title && theme && type && !geo) {
+      const channels = await prisma.channel.findMany({
+        where: {
+          name: title || "",
+          theme: theme || "",
+          language: language || "",
+          type: type || "",
         },
       });
       return new Response(JSON.stringify(channels), { status: 200 });
