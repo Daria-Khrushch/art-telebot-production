e73ee437a32f5266e2f597a8cae6c10a6bdbb37a
Daria Khrushch <73955564+Daria-Khrushch@users.noreply.github.com>
2023-06-21 22:53:57 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -4,52 +4,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "shapka-youtube.ru",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "prostomac.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "kultura.rayon.in.ua",
-        port: "",
-        pathname: "/**",
-          },
-         {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-        port: "",
-        pathname: "/**",
-          },
-            {
-        protocol: "https",
-        hostname: "play-lh.googleusercontent.com",
-        port: "",
-        pathname: "/**",
-          },
-               {
-        protocol: "https",
-        hostname: "dev.ua",
-        port: "",
-        pathname: "/**",
-          },
-                  {
-        protocol: "https",
         hostname: "*",
         port: "",
         pathname: "/**",
-          },
-    //                  {
-    //     protocol: "https",
-    //     hostname: "",
-    //     port: "",
-    //     pathname: "/**",
-    //   },
+      },
     ],
   },
 };
